aa2dbfcbfaf9f944e6aef40b4fd177eabbbf92e2
Arnab Mondal <arnab2807@gmail.com>
2019-07-25 01:26:54 +0530

    Updated config server

--- src/main/java/com/lms/svc/config/LmsConfigServerApplication.java
+++ src/main/java/com/lms/svc/config/LmsConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.lms.svc.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class LmsConfigServerApplication {
 
 	public static void main(String[] args) {
